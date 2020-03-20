782efc38297720ae2951907e1a1ca1614e1dab66
Vaannelies <0962949@hr.nl>
2020-03-20 13:19:52 +0100

    settingsActivity button werkt

--- app/src/main/java/nl/hr/annelies/fune/HomeActivity.java
+++ app/src/main/java/nl/hr/annelies/fune/HomeActivity.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class HomeActivity extends AppCompatActivity {
 
@@ -13,7 +14,7 @@ public class HomeActivity extends AppCompatActivity {
         setContentView(R.layout.activity_home);
     }
 
-    public void settings() {
+    public void settings(View view) {
         Intent i = new Intent(this, SettingsActivity.class);
         startActivity(i);
 
