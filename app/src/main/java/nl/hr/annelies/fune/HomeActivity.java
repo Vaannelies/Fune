f221ff9b08384e8062658c783f38d00445c1887f
Vaannelies <0962949@hr.nl>
2020-03-21 12:46:04 +0100

    Verder in de tutorial

--- app/src/main/java/nl/hr/annelies/fune/HomeActivity.java
+++ app/src/main/java/nl/hr/annelies/fune/HomeActivity.java
@@ -1,17 +1,32 @@
 package nl.hr.annelies.fune;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.viewpager.widget.ViewPager;
 
+import android.animation.ArgbEvaluator;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class HomeActivity extends AppCompatActivity {
+
+    ViewPager viewPager;
+    Adapter adapter;
+    List<CardModel> models;
+    Integer[] colors = null;
+    ArgbEvaluator argbEvaluator = new ArgbEvaluator();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+        models = new ArrayList<>();
+        models.add(new CardModel(R.drawable.dog, "Dog", "this is a cool dog"));
+        models.add(new CardModel(R.drawable.dog, "Dog 2", "this is also a cool dog"));
     }
 
     public void settings(View view) {
