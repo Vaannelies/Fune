358aa515fe49babd5e37a0e1f2e23f5958924afc
Vaannelies <0962949@hr.nl>
2020-03-21 13:46:10 +0100

    ViewPager werkt

--- app/src/main/java/nl/hr/annelies/fune/HomeActivity.java
+++ app/src/main/java/nl/hr/annelies/fune/HomeActivity.java
@@ -27,6 +27,50 @@ public class HomeActivity extends AppCompatActivity {
         models = new ArrayList<>();
         models.add(new CardModel(R.drawable.dog, "Dog", "this is a cool dog"));
         models.add(new CardModel(R.drawable.dog, "Dog 2", "this is also a cool dog"));
+
+        adapter = new Adapter(models, this);
+
+        viewPager = findViewById(R.id.viewPager);
+        viewPager.setAdapter(adapter);
+        viewPager.setPadding(130, 0, 130, 0);
+
+        Integer[] colors_temp =  {
+                getResources().getColor(R.color.color1),
+                getResources().getColor(R.color.color2),
+                getResources().getColor(R.color.color3),
+                getResources().getColor(R.color.color4)
+        };
+
+        colors = colors_temp;
+
+        viewPager.setOnPageChangeListener(new ViewPager.OnPageChangeListener() {
+            @Override
+            public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+                if (position < (adapter.getCount() -1) && position < (colors.length - 1)) {
+                    viewPager.setBackgroundColor(
+                            (Integer) argbEvaluator.evaluate(
+                                    positionOffset,
+                                    colors[position],
+                                    colors[position + 1]
+                            )
+                    );
+                }
+
+                else {
+                    viewPager.setBackgroundColor(colors[colors.length - 1]);
+                }
+            }
+
+            @Override
+            public void onPageSelected(int position) {
+
+            }
+
+            @Override
+            public void onPageScrollStateChanged(int state) {
+
+            }
+        });
     }
 
     public void settings(View view) {
